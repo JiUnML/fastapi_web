1e0c8ae0d823f0ab8a1f3a5af18e81203d492423
Your Name <you@example.com>
2024-01-31 13:53:30 +0900

    jinja2

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI ,Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/" , response_class=HTMLResponse)
-async def root(request: Request, ):
-    return templates.TemplateResponse( request=request, name="index.html")
+@app.get('/', response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse(request=request,name="index.html" , context={"name":"정지운"})
 
-@app.get("/hello")
+
+@app.get('/hello')
 async def hello():
-    return {"message": "안녕반가워"}
+    return "안녕! 반가워!"
