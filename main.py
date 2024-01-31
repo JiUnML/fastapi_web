8ce4345ca636e24a001739159793d0c5f09f9ed3
Your Name <you@example.com>
2024-01-31 13:17:21 +0900

    index.htnl rendering

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+@app.get("/" , response_class=HTMLResponse)
+async def root(request: Request, ):
+    return templates.TemplateResponse( request=request, name="index.html")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/hello")
+async def hello():
+    return {"message": "안녕반가워"}
